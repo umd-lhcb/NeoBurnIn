389de179d9e618c2927a07b5d835e3863117230c
Yipeng Sun <syp@umd.edu>
2017-10-18 09:39:26 -0400

    Basic structure.

---
+++ bUrnIn/server.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#
+# Last Change: Wed Oct 18, 2017 at 09:37 AM -0400
+
+
+class Server():
+    def __init__(self, port, max_connection=5, log_dir='/var/log/bUrnIn'):
+        pass
