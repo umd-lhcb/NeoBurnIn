3ee1082ff8d05d919b033e3a42a0b2e27d8629f8
Yipeng Sun <syp@umd.edu>
2017-10-18 10:36:10 -0400

    Naive implementation using socketserver module.

--- bUrnIn/client.py
+++ bUrnIn/client.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python
 #
-# Last Change: Wed Oct 18, 2017 at 09:13 AM -0400
+# Last Change: Wed Oct 18, 2017 at 10:36 AM -0400
+
+import socket
+
+
+class Client():
+    def __init__(self, host, port):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock = sock
+        self.host = host
+        self.port = port
+
+    def send(self, msg):
+        try:
+            self.sock.connect((self.host, self.port))
+            self.sock.sendall(bytes(msg + "\n", "utf-8"))
+
+        finally:
+            self.sock.close()
--- bUrnIn/server.py
+++ bUrnIn/server.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 #
-# Last Change: Wed Oct 18, 2017 at 09:37 AM -0400
+# Last Change: Wed Oct 18, 2017 at 10:29 AM -0400
+
+import socketserver
+
+
+class TCPHandler(socketserver.BaseRequestHandler):
+    '''
+    Instantiated once per connection. Handles communication between server and
+    client.
+    '''
+
+    def handle(self):
+        self.data = self.request.recv(1024).strip()
+        print("Client ip: {}".format(self.client_address[0]))
+        print(self.data)
 
 
 class Server():
-    def __init__(self, port, max_connection=5, log_dir='/var/log/bUrnIn'):
-        pass
+    def __init__(self, host, port, max_connection=5, log_dir='/var/log/bUrnIn'):
+        server = socketserver.TCPServer((host, port), TCPHandler)
+        self.server = server
