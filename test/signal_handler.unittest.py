d51d1bd7c84dd1e29df819c1f75a2fcdf33b6f0f
Yipeng Sun <syp@umd.edu>
2017-10-30 02:14:10 -0400

    A true unit test.

--- test/signal_handler.unittest.py
+++ test/signal_handler.unittest.py
@@ -1,29 +1,54 @@
 #!/usr/bin/env python
 #
-# Last Change: Mon Oct 30, 2017 at 01:35 AM -0400
+# Last Change: Mon Oct 30, 2017 at 02:13 AM -0400
 
 from time import sleep
-from datetime import datetime
+import unittest
+from multiprocessing import Process
+import os
+import signal
 
 import sys
 sys.path.insert(0, '..')
 
 from bUrnIn.server.base import BaseSignalHandler
 
-time_fmt = "%Y-%m-%d %H:%M:%S"
-
 
 class SignalHandler(BaseSignalHandler):
     def start(self):
         while not self.stop:
-            current_time = datetime.now().strftime(time_fmt)
-            print("I am still alive at: {}".format(current_time))
             sleep(1)
 
-        # Kill itself gracefully
-        print("Termination signal received, exit gracefully...")
+        # Exit with a special code for testing
+        sys.exit(13)
+
+
+def server_loop():
+    handler = SignalHandler()
+    handler.start()
+
+
+class TestKill(unittest.TestCase):
+    def test_kill_by_SIGINT(self):
+        server = Process(target=server_loop, args=())
+        server.start()
+        sleep(1.5)
+
+        os.kill(server.pid, signal.SIGINT)
+        server.join()
+
+        self.assertEqual(13, server.exitcode)
+
+    def test_kill_by_SIGTERM(self):
+        server = Process(target=server_loop, args=())
+        server.start()
+        sleep(1.5)
+
+        os.kill(server.pid, signal.SIGTERM)
+        server.join()
+
+        self.assertEqual(13, server.exitcode)
 
 
 if __name__ == "__main__":
-    handler = SignalHandler()
-    handler.start()
+    unittest.main()
