c5e1ae798a07d1309d0ba7636155f20e39ef3ac3
Yipeng Sun <syp@umd.edu>
2017-10-31 00:10:25 -0400

    Seems that multi process module can execute method function directly.

--- test/signal_handler.unittest.py
+++ test/signal_handler.unittest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Last Change: Mon Oct 30, 2017 at 03:13 PM -0400
+# Last Change: Tue Oct 31, 2017 at 12:10 AM -0400
 
 from time import sleep
 from multiprocessing import Process
@@ -18,20 +18,16 @@ from bUrnIn.server.base import BaseSignalHandler
 class SignalHandler(BaseSignalHandler):
     def start(self):
         while not self.stop:
-            sleep(1)
+            sleep(1)    # This operations is likely atomic
 
         # Exit with a special code for testing
         sys.exit(13)
 
 
-def server_loop():
-    handler = SignalHandler()
-    handler.start()
-
-
 class TestKill(unittest.TestCase):
     def test_kill_by_SIGINT(self):
-        server = Process(target=server_loop, args=())
+        handler = SignalHandler()
+        server = Process(target=handler.start, args=())
         server.start()
         sleep(1.5)
 
@@ -41,7 +37,8 @@ class TestKill(unittest.TestCase):
         self.assertEqual(13, server.exitcode)
 
     def test_kill_by_SIGTERM(self):
-        server = Process(target=server_loop, args=())
+        handler = SignalHandler()
+        server = Process(target=handler.start, args=())
         server.start()
         sleep(1.5)
 
