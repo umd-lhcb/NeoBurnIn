a6a4eecb2d851ba079e31049595c15af6153c12f
Yipeng Sun <syp@umd.edu>
2017-10-31 03:03:30 -0400

    Test that any delimiter size works.

--- test/naive_server.unittest.py
+++ test/naive_server.unittest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Last Change: Tue Oct 31, 2017 at 02:37 AM -0400
+# Last Change: Tue Oct 31, 2017 at 03:03 AM -0400
 
 from time import sleep
 from multiprocessing import Process, Queue
@@ -58,6 +58,42 @@ class TestTransferMsgSmSize(unittest.TestCase):
 
         server_port = get_free_tcp_port()
         size = 3
+
+        self.server_instance = NaiveTransmissionServer("",
+                                                       server_port, size=size)
+        self.server_instance.output = Queue()
+
+        self.server = Process(target=self.server_instance.listen, args=())
+        self.server.start()
+
+        self.client = NaiveTransmissionClient("localhost", server_port)
+
+    def test_ascii_text(self):
+        ascii_text = "Gou Li Guo Jia Sheng Si Yi"
+        self.client.send(ascii_text)
+        self.assertEqual(self.server_instance.output.get(), ascii_text)
+
+    def test_utf8_text(self):
+        utf8_text = "苟利国家生死以"
+        self.client.send(utf8_text)
+        self.assertEqual(self.server_instance.output.get(), utf8_text)
+
+    def doCleanups(self):
+        self.client.exit()
+
+        # FIXME: A dirty workaround
+        kill = Popen(["kill", "-9", str(self.server.pid)])
+        kill.wait()
+
+        self.server_instance.sock.close()
+
+
+class TestTransferMsgLgSize(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        unittest.TestCase.__init__(self, *args, **kwargs)
+
+        server_port = get_free_tcp_port()
+        size = 4096
 
         self.server_instance = NaiveTransmissionServer("",
                                                        server_port, size=size)
