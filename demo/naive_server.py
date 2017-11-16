d282198b5640fda30ed8a4588399db8a016579de
Yipeng Sun <syp@umd.edu>
2017-11-15 19:14:51 -0500

    Clean up.

--- demo/naive_server.py
+++ demo/naive_server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Last Change: Wed Nov 15, 2017 at 12:57 PM -0500
+# Last Change: Wed Nov 15, 2017 at 06:15 PM -0500
 
 import signal
 
@@ -12,6 +12,10 @@ sys.path.insert(0, '..')
 from bUrnIn.server.transmission import TransmissionServerAsync
 from bUrnIn.server.dispatcher import Dispatcher
 from bUrnIn.server.logging import LoggerForMultiProcesses
+
+
+def on_exit(signum, frame):
+    raise KeyboardInterrupt
 
 
 class NaiveDispatcher(Dispatcher):
@@ -34,14 +38,11 @@ if __name__ == "__main__":
     dispatcher.start()
 
     # Handle SIGTERM and SIGINT
-    def on_exit(signum, frame):
-        raise KeyboardInterrupt
-
     signal.signal(signal.SIGINT, on_exit)
     signal.signal(signal.SIGTERM, on_exit)
 
     # Start the TCP server on the main process
-    server = TransmissionServerAsync('localhost', 45678, msgs=msgs)
+    server = TransmissionServerAsync('localhost', 45678, msgs=msgs, logs=logs)
     server.listen()
 
     # Cleanup
