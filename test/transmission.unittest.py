b7a201a9ff9a92397924788b19c710c9c7407640
Yipeng Sun <syp@umd.edu>
2018-02-06 18:55:59 -0500

    Fix: The old unit test works.

--- test/transmission.unittest.py
+++ test/transmission.unittest.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 #
-# Last Change: Tue Feb 06, 2018 at 07:33 AM -0500
+# Last Change: Tue Feb 06, 2018 at 06:55 PM -0500
 
 import socket
 import unittest
 
 from time import sleep
 from multiprocessing import Queue
+from multiprocessing import Process as Container
 from os import kill
 from signal import SIGTERM
 
@@ -16,7 +17,7 @@ sys.path.insert(0, '..')
 from bUrnIn.framework.server import ServerAsync
 
 
-class ClientTester():
+class ClientTester(object):
     def __init__(self, host, port):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock = sock
@@ -52,58 +53,60 @@ def get_free_tcp_port():
 
 class TestTransferMsgSmSize(unittest.TestCase):
     def setUp(self):
+        self.msgs = Queue()
+
         port = get_free_tcp_port()
         size = 3
-        msgs = Queue()
-        logs = Queue()
+        server = ServerAsync("", port, self.msgs, size=size)
 
-        self.server = ServerAsync("", port, msgs, logs, size=size)
-        self.server.start()
+        self.container = Container(target=server.start)
+        self.container.start()
         sleep(0.5)  # Need this to make sure server is properly initialized
 
-        self.client = TransmissionClientTester("localhost", port)
+        self.client = ClientTester("localhost", port)
 
     def test_ascii_text(self):
         ascii_text = "Gou Li Guo Jia Sheng Si Yi"
         self.client.send(ascii_text)
-        self.assertEqual(self.server.msgs.get(), ascii_text)
+        self.assertEqual(self.msgs.get(), ascii_text)
 
     def test_utf8_text(self):
         utf8_text = "苟利国家生死以"
         self.client.send(utf8_text)
-        self.assertEqual(self.server.msgs.get(), utf8_text)
+        self.assertEqual(self.msgs.get(), utf8_text)
 
     def doCleanups(self):
         self.client.exit()
-        kill(self.server.server_process.pid, SIGTERM)
+        kill(self.container.pid, SIGTERM)
 
 
 class TestTransferMsgLgSize(unittest.TestCase):
     def setUp(self):
+        self.msgs = Queue()
+
         port = get_free_tcp_port()
         size = 81920
-        msgs = Queue()
-        logs = Queue()
+        server = ServerAsync("", port, self.msgs, size=size)
 
-        self.server = TransmissionServerAsync("", port, msgs, logs, size=size)
-        self.server.start()
+        self.container = Container(target=server.start)
+        self.container.start()
         sleep(0.5)  # Need this to make sure server is properly initialized
 
-        self.client = TransmissionClientTester("localhost", port)
+        self.client = ClientTester("localhost", port)
 
     def test_ascii_text(self):
         ascii_text = "Gou Li Guo Jia Sheng Si Yi"
         self.client.send(ascii_text)
-        self.assertEqual(self.server.msgs.get(), ascii_text)
+        self.assertEqual(self.msgs.get(), ascii_text)
 
     def test_utf8_text(self):
         utf8_text = "苟利国家生死以"
         self.client.send(utf8_text)
-        self.assertEqual(self.server.msgs.get(), utf8_text)
+        self.assertEqual(self.msgs.get(), utf8_text)
 
     def doCleanups(self):
         self.client.exit()
-        kill(self.server.server_process.pid, SIGTERM)
+        kill(self.container.pid, SIGTERM)
 
 
 if __name__ == "__main__":
