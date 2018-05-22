3c02e2d3f391134d20b8a5bc4f5d31aebb12f30c
Yipeng Sun <syp@umd.edu>
2018-05-22 17:21:33 -0400

    More elegant unit testings.

--- test/transmission.unittest.py
+++ test/transmission.unittest.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 #
-# Last Change: Fri May 11, 2018 at 02:05 PM -0400
+# Last Change: Tue May 22, 2018 at 05:17 PM -0400
 
 import socket
 import unittest
 
 from time import sleep
-from multiprocessing import Queue
 from multiprocessing import Process as Container
 from multiprocessing import Event
+from multiprocessing import Queue
 from os import kill
 from signal import SIGTERM
 
@@ -53,74 +53,94 @@ class ClientTester(object):
         self.sock.close()
 
 
-class TestTransferMsgSmSize(unittest.TestCase):
-    def setUp(self):
-        self.msgs = Queue()
+class MockLogger(object):
+    def __init__(self, log_queue):
+        self.logs = log_queue
 
-        port = get_free_tcp_port()
-        size = 3
-        server = ServerAsync("", port, self.msgs, size=size)
-        wait_event = Event()
+    def info(self, msg):
+        pass  # We don't need info-level logging
 
-        self.container = Container(target=server.run, args=(wait_event,))
-        self.container.start()
+    def error(self, msg):
+        self.logs.put(msg)
 
-        # Need this to make sure server is properly initialized
-        wait_event.wait()
-        self.client = ClientTester("localhost", port)
-
-    def test_ascii_text(self):
-        ascii_text = "Gou Li Guo Jia Sheng Si Yi"
-        self.client.send(ascii_text)
-        self.assertEqual(self.msgs.get(), ascii_text)
-
-    def test_utf8_text(self):
-        utf8_text = "苟利国家生死以"
-        self.client.send(utf8_text)
-        self.assertEqual(self.msgs.get(), utf8_text)
-
-    def doCleanups(self):
-        self.client.exit()
-        kill(self.container.pid, SIGTERM)
+    def warning(self, msg):
+        self.logs.put(msg)
 
 
-class TestTransferMsgLgSize(unittest.TestCase):
-    def setUp(self):
-        self.msgs = Queue()
+# class TestTransferMsgSmSize(unittest.TestCase):
+    # def setUp(self):
+        # self.msgs = Queue()
 
-        port = get_free_tcp_port()
-        size = 81920
-        server = ServerAsync("", port, self.msgs, size=size)
-        wait_event = Event()
+        # port = get_free_tcp_port()
+        # size = 3
+        # server = ServerAsync("", port, self.msgs, size=size)
+        # wait_event = Event()
 
-        self.container = Container(target=server.run, args=(wait_event,))
-        self.container.start()
+        # self.container = Container(target=server.run, args=(wait_event,))
+        # self.container.start()
 
-        wait_event.wait()
-        self.client = ClientTester("localhost", port)
+        # # Need this to make sure server is properly initialized
+        # wait_event.wait()
+        # self.client = ClientTester("localhost", port)
 
-    def test_ascii_text(self):
-        ascii_text = "Gou Li Guo Jia Sheng Si Yi"
-        self.client.send(ascii_text)
-        self.assertEqual(self.msgs.get(), ascii_text)
+    # def test_ascii_text(self):
+        # ascii_text = "Gou Li Guo Jia Sheng Si Yi"
+        # self.client.send(ascii_text)
+        # self.assertEqual(self.msgs.get(), ascii_text)
 
-    def test_utf8_text(self):
-        utf8_text = "苟利国家生死以"
-        self.client.send(utf8_text)
-        self.assertEqual(self.msgs.get(), utf8_text)
+    # def test_utf8_text(self):
+        # utf8_text = "苟利国家生死以"
+        # self.client.send(utf8_text)
+        # self.assertEqual(self.msgs.get(), utf8_text)
 
-    def doCleanups(self):
-        self.client.exit()
-        kill(self.container.pid, SIGTERM)
+    # def doCleanups(self):
+        # self.client.exit()
+        # kill(self.container.pid, SIGTERM)
+
+
+# class TestTransferMsgLgSize(unittest.TestCase):
+    # def setUp(self):
+        # self.msgs = Queue()
+
+        # port = get_free_tcp_port()
+        # size = 81920
+        # server = ServerAsync("", port, self.msgs, size=size)
+        # wait_event = Event()
+
+        # self.container = Container(target=server.run, args=(wait_event,))
+        # self.container.start()
+
+        # wait_event.wait()
+        # self.client = ClientTester("localhost", port)
+
+    # def test_ascii_text(self):
+        # ascii_text = "Gou Li Guo Jia Sheng Si Yi"
+        # self.client.send(ascii_text)
+        # self.assertEqual(self.msgs.get(), ascii_text)
+
+    # def test_utf8_text(self):
+        # utf8_text = "苟利国家生死以"
+        # self.client.send(utf8_text)
+        # self.assertEqual(self.msgs.get(), utf8_text)
+
+    # def doCleanups(self):
+        # self.client.exit()
+        # kill(self.container.pid, SIGTERM)
 
 
 class TestTransTimeout(unittest.TestCase):
     def setUp(self):
+        self.text = "苟利国家生死以"
+
         self.msgs = Queue()
-        self.text = "Gou Li Guo Jia Sheng Si Yi"
+
+        # We mock a logger to grab all logs generated in the subprocesses
+        self.logs = Queue()
+        self.logger = MockLogger(self.logs)
 
         port = get_free_tcp_port()
         server = ServerAsync("", port, self.msgs, max_retries=2, timeout=0.1)
+        server.logger = self.logger
         wait_event = Event()
 
         self.container = Container(target=server.run, args=(wait_event,))
@@ -130,20 +150,31 @@ class TestTransTimeout(unittest.TestCase):
         self.client = ClientTester("localhost", port)
 
     def test_timeout_error(self):
-        print('\nTesting Timeout, expecting 3 log lines:')
         self.client.send(self.text, delay=0.5)
         self.assertEqual(self.msgs.qsize(), 0)
+        self.assertEqual(self.logs.get(),
+                         'TimeoutError: Failed to receive: 1 time(s).')
+        self.assertEqual(self.logs.get(),
+                         'TimeoutError: Failed to receive: 2 time(s).')
+        self.assertEqual(self.logs.get(),
+                         'TimeoutError: Maximum retries exceeded. Transmission failed.')
 
     def test_timeout_once(self):
-        print('\nTesting Timeout once, expecting 1 log line:')
-        self.client.send(self.text, delay=0.2)
+        self.client.send(self.text, delay=0.12)
         self.assertEqual(self.msgs.get(), self.text)
+        self.assertEqual(self.logs.get(),
+                         'TimeoutError: Failed to receive: 1 time(s).')
 
     def test_msg_corrupted(self):
-        print('\nTesting corrupted msg, expecting 3 log lines:')
         self.client.send(self.text, corrupted=True)
-        sleep(0.3)
+        sleep(0.4)
         self.assertEqual(self.msgs.qsize(), 0)
+        self.assertEqual(self.logs.get(),
+                         'TimeoutError: Failed to receive: 1 time(s).')
+        self.assertEqual(self.logs.get(),
+                         'TimeoutError: Failed to receive: 2 time(s).')
+        self.assertEqual(self.logs.get(),
+                         'TimeoutError: Maximum retries exceeded. Transmission failed.')
 
     def doCleanups(self):
         self.client.exit()
