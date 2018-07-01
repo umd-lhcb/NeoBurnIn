003198b3497b2a1814f5243d7121bda048e63754
Yipeng Sun <syp@umd.edu>
2018-07-01 00:10:00 -0400

    Migrate declarations for logging config.

--- NeoBurnIn/io/logging.py
+++ NeoBurnIn/io/logging.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 #
-# Last Change: Fri Jun 29, 2018 at 12:12 AM -0400
+# Last Change: Sun Jul 01, 2018 at 12:08 AM -0400
 # Too bad. Impurities everywhere.
 
 import logging
 import logging.config
 import logging.handlers
 
-from BurnIn.base import standard_time_format
+from NeoBurnIn.base import standard_time_format
+from NeoBurnIn.base import nested_dict
 
 
 ###################
@@ -20,7 +21,26 @@ def parse_size_limit(size):
     return int(size_parsed[0]) * size_dict[size_parsed[1]]
 
 
-def configure_logger(config_dict):
+def configure_logger(
+    log_file,
+    src_addr, dst_addrs, credentials,
+    data_file, data_file_max_size, data_file_backup_count
+):
+    config_dict = nested_dict()
+
+    log_default_version(config_dict)
+    log_formatter_detailed(config_dict)
+
+    log_handler_console(config_dict)
+    log_handler_file(config_dict, log_file)
+    log_handler_email(config_dict, src_addr, dst_addrs, credentials)
+    log_handler_data(config_dict,
+                     data_file,
+                     data_file_max_size, data_file_backup_count)
+
+    config_dict['loggers']['log']  = {'handlers': ['console', 'file', 'email']}
+    config_dict['loggers']['data'] = {'handlers': ['datafile']}
+
     logging.config.dictConfig(config_dict)
 
 
@@ -45,13 +65,32 @@ def log_formatter_detailed(config_dict):
 # Handlers #
 ############
 
+def log_handler_console(config_dict, level='WARNING'):
+    settings = {
+        'level': level,
+        'class': 'logging.StreamHandler'
+    }
+    config_dict['handlers']['console'] = settings
+
+
+def log_handler_file(config_dict, log_file, formatter='detailed'):
+    settings = {
+        'level': 'INFO',
+        'class': 'logging.FileHandler',
+        'filename': log_file,
+        'formatter': formatter
+    }
+    config_dict['handlers']['file'] = settings
+
+
 def log_handler_email(config_dict,
                       src_addr, dst_addrs,
                       credentials,
+                      level='CRITICAL',
                       email_title='[BurnIn]: Summary / An error has occurred'
                       ):
     settings = {
-        'level': 'CRITICAL',
+        'level': level,
         'class': 'logging.handlers.SMTPHandler',
         'formatter': 'detailed',
         'fromaddr': src_addr,
@@ -62,3 +101,17 @@ def log_handler_email(config_dict,
         'secure': ()
     }
     config_dict['handlers']['email'] = settings
+
+
+# This is for data logging
+def log_handler_data(config_dict,
+                     data_file,
+                     data_file_max_size, data_file_backup_count):
+    settings = {
+        'level': 'INFO',
+        'class': 'logging.handlers.RotatingFileHandler',
+        'filename': data_file,
+        'maxBytes': parse_size_limit(data_file_max_size),
+        'backupCount': int(data_file_backup_count)
+    }
+    config_dict['handlers']['datafile'] = settings
