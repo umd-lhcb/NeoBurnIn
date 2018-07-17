f4b9b9679e53cb58897cfcba951c40077e765849
Yipeng Sun <syp@umd.edu>
2018-07-17 11:00:54 -0400

    WIP: convert client to aiohttp as well.

--- demo/naive_client.py
+++ demo/naive_client.py
@@ -1,53 +1,45 @@
 #!/usr/bin/env python
 #
-# Last Change: Tue Jun 12, 2018 at 03:20 AM -0400
+# Last Change: Tue Jul 17, 2018 at 11:00 AM -0400
 
-import socket
+import aiohttp
 
 from datetime import datetime
 from random import uniform
+from time import sleep
 
 import sys
 sys.path.insert(0, '..')
 
 
+def get_current_datetime():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+
+
+def msg_gen(ch_name):
+    return get_current_datetime() + '|' + ch_name + '|' + str(uniform(1, 10))
+
+
 class NaiveClient():
-    EOM = 'EOM'
+    def __init__(self, ip='localhost', port='45678'):
+        self.url = 'http://{}:{}/datacollect'.format(ip, port)
 
-    def __init__(self, ip, port):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.ip = ip
-        self.port = port
-
-    def send(self, msg):
-        try:
-            self.sock.connect((self.ip, self.port))
-            self.sock.send(bytes(msg, 'utf-8'))
-            self.sock.send(bytes(self.EOM, 'utf-8'))
-
-        finally:
-            self.sock.close()
+    async def send(self, msg):
+        async with aiohttp.ClientSession() as session:
+            async with session.put(self.url,
+                                   data=bytearray(msg, 'utf8')) as resp:
+                print(resp.status)
+                print(await resp.text())
 
 
 if __name__ == "__main__":
+    client = NaiveClient()
     while True:
         try:
-            date = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-
-            dataset = dict()
-            dataset[sys.argv[2]] = (date, str(uniform(1, 10)))
-
-            msg = ''
-            for key in dataset.keys():
-                msg = msg + dataset[key][0] + '|'
-                msg = msg + key + '|'
-                msg = msg + dataset[key][1] + '\n'
-
-            print(msg.strip('\n'))
-
-            client = NaiveClient(sys.argv[1], 45678)
+            msg = msg_gen(sys.argv[1])
+            print(msg)
             client.send(msg)
-            break
+            sleep(1)
 
         except KeyboardInterrupt:
             break
