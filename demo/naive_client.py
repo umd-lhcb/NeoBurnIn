099d23504626b7f546f099f7f944614d7fb71108
Yipeng Sun <syp@umd.edu>
2018-07-18 10:45:55 -0400

    Correctly multiple data collecting threads.

--- demo/naive_client.py
+++ demo/naive_client.py
@@ -1,34 +1,33 @@
 #!/usr/bin/env python
 #
-# Last Change: Tue Jul 17, 2018 at 04:07 PM -0400
+# Last Change: Wed Jul 18, 2018 at 10:45 AM -0400
 
 import asyncio
 import aiohttp
 
 from random import uniform
-from time import sleep
 from queue import Queue
-from threading import Thread
+from threading import Thread, Event
 
 import sys
 sys.path.insert(0, '..')
 
-from NeoBurnIn.base import time_now_formatted
+from NeoBurnIn.base import time_now_formatted, ThreadTerminator
 from NeoBurnIn.base import BaseDataSource, BaseClient
 
 
 class NaiveRandDataSource(BaseDataSource):
-    def __init__(self, queue, ch_name='ch1'):
+    def __init__(self, queue, stop_event, ch_name='ch1'):
         self.queue = queue
+        self.stop_event = stop_event
         self.ch_name = ch_name
 
     def start(self, interval):
-        self.thread = Thread(target=self.run, args=(interval,))
+        self.thread = Thread(target=self.run, args=(stop_event, interval))
         self.thread.start()
 
-    def run(self, interval):
-        while True:
-            sleep(interval)
+    def run(self, stop_event, interval):
+        while not stop_event.wait(interval):
             msg = self.get()
             print(msg)
             self.queue.put(msg)
@@ -38,20 +37,27 @@ class NaiveRandDataSource(BaseDataSource):
             str(uniform(1, 10))
 
 
-class NaiveClient(BaseClient):
-    def __init__(self, queue, ip='localhost', port='45678'):
+class NaiveClient(ThreadTerminator, BaseClient):
+    def __init__(self, queue, *args,
+                 ip='localhost', port='45678', **kwargs):
         self.queue = queue
 
         self.url = 'http://{}:{}/datacollect'.format(ip, port)
         self.loop = asyncio.get_event_loop()
 
+        super().__init__(*args, **kwargs)
+
     def run(self):
         while True:
             # NOTE: this is a blocking call, which means that all messages will
             # be sent sequentially---no concurrency.
-            msg = self.queue.get()
-            data = bytearray(msg, 'utf8')
-            self.loop.run_until_complete(self.post(data))
+            try:
+                msg = self.queue.get()
+                data = bytearray(msg, 'utf8')
+                self.loop.run_until_complete(self.post(data))
+            except KeyboardInterrupt:
+                super().killall()
+                break
 
     async def post(self, data):
         async with aiohttp.ClientSession() as client:
@@ -64,8 +70,13 @@ class NaiveClient(BaseClient):
 
 if __name__ == "__main__":
     data_queue = Queue()
-    datasource = NaiveRandDataSource(data_queue)
-    client = NaiveClient(data_queue)
+    stop_event = Event()
+    thread_list = []
 
+    datasource = NaiveRandDataSource(data_queue, stop_event)
     datasource.start(1)
+
+    thread_list.append(datasource.thread)
+
+    client = NaiveClient(data_queue, stop_event, thread_list=thread_list)
     client.run()
