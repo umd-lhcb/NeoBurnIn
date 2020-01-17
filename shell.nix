7d0c4dae646c0e4ee15d150f078a954aeb4cfe22
Yipeng Sun <syp@umd.edu>
2020-01-17 06:55:20 -0500

    Add a nix-shell.

---
+++ shell.nix
@@ -0,0 +1,27 @@
+let
+  pkgs = import <nixpkgs> {};
+  python = pkgs.python3;
+  pythonPackages = python.pkgs;
+in
+
+pkgs.mkShell {
+  name = "pip-env";
+  buildInputs = with pythonPackages; [
+    # Python requirements (enough to get a virtualenv going).
+    virtualenvwrapper
+  ];
+
+  shellHook = ''
+    # Allow the use of wheels.
+    SOURCE_DATE_EPOCH=$(date +%s)
+
+    VENV=./.virtualenv
+    if test ! -d $VENV; then
+      virtualenv $VENV
+    fi
+    source $VENV/bin/activate
+
+    # allow for the environment to pick up packages installed with virtualenv
+    export PYTHONPATH=$VENV/${python.sitePackages}/:$PYTHONPATH
+  '';
+}
