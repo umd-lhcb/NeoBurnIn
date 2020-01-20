8706a5d476e29dcdf74a8774dd691c4a59873bb3
Yipeng Sun <syp@umd.edu>
2020-01-20 02:11:15 -0500

    Define unit tests.

---
+++ test/test_functional.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+#
+# Last Change: Mon Jan 20, 2020 at 01:55 AM -0500
+
+import pytest
+
+import sys
+sys.path.insert(0, '..')
+
+from NeoBurnIn.base import DataPassthru
+from NeoBurnIn.functional import name, parse_directive
+
+
+################
+# For functors #
+################
+
+@pytest.fixture
+def data():
+    return DataPassthru('2020-01-20', 'TEST_CASE1', 3.1415)
+
+
+def test_name_match_non_terminal(data):
+    assert name(data, 'TEST_CASE\\d+') == data
+
+
+def test_name_match_terminal(data):
+    assert name(data, 'TEST_CASE\\d+', final=True) is True
+
+
+def test_name_not_match_non_terminal(data):
+    assert name(data, 'TEAST_CAS') is None
+
+
+def test_name_not_match_terminal(data):
+    assert name(data, 'TEAST_CAS', final=True) is False
+
+
+############################
+# For functor construction #
+############################
+
+@pytest.fixture
+def data_alt():
+    return DataPassthru('2020-01-20', 'TEST_CASE1', 6)
+
+
+def test_directive1(data_alt):
+    directive = [
+        {
+            'match': {'name': "TEST_CASE\\d+", "valueGt": 5},
+            'action': {'sink': 'tester1', 'state': 'off', 'ch': 1}
+        }]
+    parsed = parse_directive(directive)
+    combined_functor = list(parsed.keys())[0]
+
+    assert combined_functor(data_alt) is True
+
+
+def test_directive2(data_alt):
+    directive = [
+        {
+            'match': {'name': "TEST_CASE\\d+", "valueGt": 7},
+            'action': {'sink': 'tester1', 'state': 'off', 'ch': 1}
+        }]
+    parsed = parse_directive(directive)
+    combined_functor = list(parsed.keys())[0]
+
+    assert combined_functor(data_alt) is False
+
+
+def test_directive3(data_alt):
+    directive = [
+        {
+            'match': {'name': "TESAT_CASE\\d+", "valueGt": 5},
+            'action': {'sink': 'tester1', 'state': 'off', 'ch': 1}
+        }]
+    parsed = parse_directive(directive)
+    combined_functor = list(parsed.keys())[0]
+
+    assert combined_functor(data_alt) is False
