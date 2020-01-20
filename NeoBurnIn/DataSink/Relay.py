734c8b9f1a4be23f0e96eb69b47c2bb84fda3065
Yipeng Sun <syp@umd.edu>
2020-01-20 06:00:12 -0500

    WIP: Actual USB relay control.

---
+++ NeoBurnIn/DataSink/Relay.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#
+# Last Change: Mon Jan 20, 2020 at 05:58 AM -0500
+
+import logging
+
+from rpi.burnin.USBRelay import get_all_device_paths
+from NeoBurnIn.base import BaseDataSink
+
+logger = logging.getLogger(__name__)
+
+
+class RelaySink(BaseDataSink):
+    def __init__(self, host, port, entry='relay',
+                 automaticDiscovery=True, relayIdx=0, relayDevId=None):
+        self.host = host
+        self.port = port
+        self.entry = entry
+
+        if automaticDiscovery:
+            relays = list(map(lambda x: x.decode('utf8'),
+                              get_all_device_paths()))
+            self.relay = relays[relayIdx]
+        else:
+            self.relay = relayDevId
+
+    def on(self, ch):
+        return 'http://{}:{}/{}/{}/{}/{}'.format(
+            self.host, self.port, self.entry, self.relay, ch, 'on'
+        )
+
+    def off(self, ch):
+        return 'http://{}:{}/{}/{}/{}/{}'.format(
+            self.host, self.port, self.entry, self.relay, ch, 'off'
+        )
