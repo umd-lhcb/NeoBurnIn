b4779eadf12e352b9c1c497d3923fe8474f530a2
Yipeng Sun <syp@umd.edu>
2020-02-07 22:48:30 +0800

    WIP #4: Fix some small bugs.

--- NeoBurnIn/DataSource/Current.py
+++ NeoBurnIn/DataSource/Current.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 #
-# Last Change: Fri Feb 07, 2020 at 09:21 PM +0800
+# Last Change: Fri Feb 07, 2020 at 10:40 PM +0800
 
 import logging
 
+from threading import Thread
 from labSNMP.wrapper.Wiener import WienerControl
 
 from NeoBurnIn.base import BaseDataSource, DataPassthru
@@ -12,9 +13,9 @@ from NeoBurnIn.base import time_now_formatted
 logger = logging.getLogger(__name__)
 
 
-class CurrentMaratonDataSource(BaseDataSource):
-    def __init__(self, stop_event, queue, interval, ipAddr,
-                 displayName=None, psuChannels=list()):
+class CurrentMaratonDataSource(Thread, BaseDataSource, ):
+    def __init__(self, stop_event, queue, interval, ipAddr, *args,
+                 displayName=None, psuChannels=list(), **kwargs):
         self.stop_event = stop_event
         self.queue = queue
         self.interval = interval
@@ -22,6 +23,8 @@ class CurrentMaratonDataSource(BaseDataSource):
         self.psuChannels = psuChannels
 
         self.psu = WienerControl(ipAddr)
+
+        super().__init__(*args, **kwargs)
 
     def run(self):
         while not self.stop_event.wait(self.interval):
@@ -37,3 +40,6 @@ class CurrentMaratonDataSource(BaseDataSource):
                 DataPassthru(time_now_formatted(), self.displayName+str(ch),
                              curr))
         return result
+
+    def cleanup(self):
+        self.join()
