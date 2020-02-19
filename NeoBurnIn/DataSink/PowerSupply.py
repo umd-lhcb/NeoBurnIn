665d177119de1b6380470ad34962b94b181b19b4
Yipeng Sun <syp@umd.edu>
2020-02-19 23:54:57 +0800

    OCD: Consistent variable naming.

--- NeoBurnIn/DataSink/PowerSupply.py
+++ NeoBurnIn/DataSink/PowerSupply.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Last Change: Wed Feb 19, 2020 at 10:58 PM +0800
+# Last Change: Wed Feb 19, 2020 at 11:54 PM +0800
 
 import logging
 
@@ -13,15 +13,15 @@ class PowerSupplySink(BaseDataSink):
     def __init__(self, host, port, psu_ip, entry='psu'):
         self.host = host
         self.port = port
-        self.psu_ip = psu_ip
+        self.psuIp = psuIp
         self.entry = entry
 
     def on(self, ch):
         return 'http://{}:{}/{}/{}/{}/{}'.format(
-            self.host, self.port, self.entry, self.psu_ip, ch, 'on'
+            self.host, self.port, self.entry, self.psuIp, ch, 'on'
         )
 
     def off(self, ch):
         return 'http://{}:{}/{}/{}/{}/{}'.format(
-            self.host, self.port, self.entry, self.psu_ip, ch, 'off'
+            self.host, self.port, self.entry, self.psuIp, ch, 'off'
         )
