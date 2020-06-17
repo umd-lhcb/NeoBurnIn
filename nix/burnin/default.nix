8666e389f799ef73b85a2e6637c91b5a1fd34846
Yipeng Sun <syp@umd.edu>
2020-06-17 15:08:30 +0800

    No need to create a virtualenv for this.

--- nix/burnin/default.nix
+++ nix/burnin/default.nix
@@ -4,7 +4,8 @@ let
   pythonOverrides = {
     packageOverrides = self: super: {
       build_utils = super.callPackage ./build_utils {};
-      RPi_GPIO = super.callPackage ./fake_rpi {};
+      RPi_GPIO = super.callPackage ./RPi.GPIO {};
+      fake_rpi = super.callPackage ./fake_rpi {};
       rpi_burnin = super.callPackage ./rpi.burnin {};
       rainbow_logging_handler = super.callPackage ./rainbow_logging_handler {};
     };
--- shell.nix
+++ shell.nix
@@ -7,9 +7,6 @@ in
 pkgs.mkShell {
   name = "pip-env";
   buildInputs = with pythonPackages; [
-    # Python requirements (enough to get a virtualenv going).
-    virtualenvwrapper
-
     hidapi
 
     aiohttp
@@ -21,20 +18,4 @@ pkgs.mkShell {
     RPi_GPIO
     rainbow_logging_handler
   ];
-
-  shellHook = ''
-    # Allow the use of wheels.
-    SOURCE_DATE_EPOCH=$(date +%s)
-
-    if test -d $HOME/build/python-venv; then
-      VENV=$HOME/build/python-venv/NeoBurnIn
-    else
-      VENV=./.virtualenv
-    fi
-
-    if test ! -d $VENV; then
-      virtualenv $VENV
-    fi
-    source $VENV/bin/activate
-  '';
 }
