82696da6388ee3fdc03e7f1681ff8411e4880bb7
Yipeng Sun <syp@umd.edu>
2020-06-17 18:28:55 +0800

    Bump rpi.burnin version.

--- nix/burnin/rpi.burnin/default.nix
+++ nix/burnin/rpi.burnin/default.nix
@@ -2,17 +2,15 @@
 
 buildPythonPackage rec {
   pname = "rpi.burnin";
-  version = "0.3";
+  version = "0.3.1";
 
   src = fetchgit {
     url = "https://github.com/umd-lhcb/${pname}";
     rev = version;
-    sha256 = "13zbrhg1qwmn2q4w0spphvaan4bqfh1g6iq0dmsilnr6g601m7jd";
+    sha256 = "1771dhib3vfcn4zshc9sgw6m3xnhzrd8pbksgjw6ahac2crxd995";
   };
 
-  propagatedBuildInputs = [
-    hidapi
-  ]
+  propagatedBuildInputs = [ hidapi ]
   ++ stdenv.lib.optionals (stdenv.isDarwin) [ fake_rpi ]
   ++ stdenv.lib.optionals (!stdenv.isDarwin) [ RPi_GPIO ]
   ;
