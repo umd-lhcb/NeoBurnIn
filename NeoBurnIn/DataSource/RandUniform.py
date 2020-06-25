328008c6624b08f2c451b5333644b0df7a3d17d9
Yipeng Sun <syp@umd.edu>
2020-06-26 03:04:11 +0800

    Use random uniform source to mock psu source.

--- NeoBurnIn/DataSource/RandUniform.py
+++ NeoBurnIn/DataSource/RandUniform.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Last Change: Mon Jan 20, 2020 at 04:50 AM -0500
+# Last Change: Fri Jun 26, 2020 at 02:35 AM +0800
 
 import logging
 
@@ -15,11 +15,11 @@ logger = logging.getLogger(__name__)
 
 class RandUniformDataSource(Thread, BaseDataSource):
     def __init__(self, stop_event, queue, *args,
-                 interval=1, chName='RAND', **kwargs):
+                 interval=1, displayName='RAND', **kwargs):
         self.queue = queue
         self.stop_event = stop_event
         self.interval = interval
-        self.chName = chName
+        self.displayName = displayName
 
         super().__init__(*args, **kwargs)
 
@@ -34,5 +34,36 @@ class RandUniformDataSource(Thread, BaseDataSource):
         logging.debug('Data source closed.')
 
     def get(self):
-        return DataPassthru(time_now_formatted(), self.chName,
+        return DataPassthru(time_now_formatted(), self.displayName,
                             uniform(1, 10))
+
+
+class RandUniformDataSourceMul(Thread, BaseDataSource):
+    def __init__(self, stop_event, queue, *args,
+                 interval=1, displayName='RAND', psuChannels=list(range(1, 12)),
+                 **kwargs):
+        self.queue = queue
+        self.stop_event = stop_event
+        self.interval = interval
+        self.displayName = displayName
+        self.psuChannels = psuChannels
+
+        super().__init__(*args, **kwargs)
+
+    def run(self):
+        while not self.stop_event.wait(self.interval):
+            currs = self.get()
+            for c in currs:
+                self.queue.put(c)
+
+    def cleanup(self, timeout=10):
+        self.join(timeout)
+        logging.debug('Data source closed.')
+
+    def get(self):
+        result = []
+        for ch in self.psuChannels:
+            result.append(
+                DataPassthru(time_now_formatted(), self.displayName+str(ch),
+                             uniform(1, 2)))
+        return result
