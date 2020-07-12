c6cbabcacdcdf1eaeb995eb3797d0f2f55ec0916
Yipeng Sun <syp@umd.edu>
2020-07-12 22:05:35 +0800

    WIP #17: Naive log parser that works.

--- plot/LogParser.py
+++ plot/LogParser.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python
 #
-# Last Change: Sun Jul 12, 2020 at 09:36 PM +0800
+# Last Change: Sun Jul 12, 2020 at 10:05 PM +0800
 
 from argparse import ArgumentParser, Action
 from datetime import datetime
 from sys import exit
 from re import search
 from collections import defaultdict as ddict
+from os import makedirs
+from pathlib import Path
+from csv import writer
+
+import sys
+sys.path.insert(0, '..')
+
+from NeoBurnIn.base import standard_time_format
 
 
 ###################
@@ -33,7 +41,8 @@ def parse_input():
 specify the path to burn-in log file.
     ''')
 
-    parser.add_argument('output_dir',
+    parser.add_argument('-o', '--output-dir',
+                        default='.',
                         help='''
 specify the output directory.
     ''')
@@ -63,11 +72,36 @@ The date format should be of the following format: YYYY-MM-DD:HH.
 
 
 def extract_data(log_file, begin, end):
-    result = ddict(lambda: list([['time', 'value']]))
+    data = ddict(lambda: list([['time', 'value']]))
 
     with open(log_file) as f:
-        pass
+        for l in f:
+            match = search(r'Received: (.*)$', l)
+            if match:
+                time, name, value = match.group(1).split('|')
+
+                time_obj = datetime.strptime(time, standard_time_format)
+
+                if begin <= time_obj <= end:
+                    data[name].append([time, float(value)])
+
+    return data
+
+
+def write_data(output_dir, data, begin, end):
+    output_dir = Path(output_dir) / Path('{}_{}'.format(
+        begin.strftime(TIME_FMT), end.strftime(TIME_FMT)
+    ))
+    makedirs(output_dir, exist_ok=True)
+
+    for filename, value in data.items():
+        with open(output_dir / Path(filename+'.csv'), 'w') as f:
+            data_writer = writer(f)
+            data_writer.writerows(value)
 
 
 if __name__ == '__main__':
     args = parse_input()
+
+    data = extract_data(args.log_file, args.begin, args.end)
+    write_data(args.output_dir, data, args.begin, args.end)
