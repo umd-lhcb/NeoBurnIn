5bc1e6bf94eb10d5aa9286f538d6d295984acc99
Zishuo Yang <yangzs@umd.edu>
2020-07-13 15:10:14 -0400

    Update switching load board control

--- demo/SwLoadTest.py
+++ demo/SwLoadTest.py
@@ -11,22 +11,29 @@ import time
 GPIO.setmode(GPIO.BOARD)
 
 # Take GPIO pin from user input
-ch1 = int(sys.argv[1])
-ch2 = int(sys.argv[2])
+#ch1 = int(sys.argv[1])
+#ch2 = int(sys.argv[2])
 
+ch1,ch2,ch3,ch4 = 13,15,18,32
 GPIO.setup(ch1, GPIO.OUT)
 GPIO.setup(ch2, GPIO.OUT)
+GPIO.setup(ch3, GPIO.OUT)
+GPIO.setup(ch4, GPIO.OUT)
 
 try:
     while True:
-        print("Pulling GPIO pins {} and {} HIGH".format(ch1, ch2))
+        print("Pulling GPIO pins HIGH")
         GPIO.output(ch1, GPIO.HIGH)
         GPIO.output(ch2, GPIO.HIGH)
+        GPIO.output(ch3, GPIO.HIGH)
+        GPIO.output(ch4, GPIO.HIGH)
         time.sleep(60)
 
-        print("Pulling GPIO pins {} and {} LOW".format(ch1, ch2))
+        print("Pulling GPIO pins LOW")
         GPIO.output(ch1, GPIO.LOW)
         GPIO.output(ch2, GPIO.LOW)
+        GPIO.output(ch3, GPIO.LOW)
+        GPIO.output(ch4, GPIO.LOW)
         time.sleep(10)
 except KeyboardInterrupt:
     print("Stopping test for switching load board.")
