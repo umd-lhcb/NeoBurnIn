82597fd687727f3695fdcc5acab59485b4c049a4
Yipeng Sun <syp@umd.edu>
2020-07-21 00:29:14 +0800

    Update unit tests.

--- test/test_io_server.py
+++ test/test_io_server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Last Change: Mon Jun 29, 2020 at 02:42 AM +0800
+# Last Change: Tue Jul 21, 2020 at 12:29 AM +0800
 
 import pytest
 
@@ -25,14 +25,18 @@ def stash():
 def test_initial_value(stash):
     assert stash['overall'] == {
         'time': [],
-        'data': []
+        'data': [],
+        'data_hi': [],
+        'data_lo': [],
     }
 
 
 def test_default_value_to_a_new_key(stash):
     assert stash['something'] == {
         'time': DataStream(max_length=1000),
-        'data': DataStats(max_length=1000)
+        'data': DataStream(max_length=1000),
+        'data_hi': DataStats(max_length=1000),
+        'data_lo': DataStats(max_length=1000),
     }
 
 
