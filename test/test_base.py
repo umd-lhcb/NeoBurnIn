8bb193591b0d80c9c4661c4500613975cbeb6e44
Yipeng Sun <syp@umd.edu>
2020-08-26 02:43:22 +0800

    WIP #19: Catch a bug in test case that list is not properly marked as full.

--- test/test_base.py
+++ test/test_base.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Last Change: Mon Jul 20, 2020 at 12:46 AM +0800
+# Last Change: Wed Aug 26, 2020 at 02:43 AM +0800
 
 import pytest
 import statistics
@@ -55,6 +55,11 @@ def test_data_stream_return_val_of_append(data_stream):
 def test_data_stream_pass_generator_as_an_argument():
     data_stream = DataStream(range(0, 6), max_length=5)
     assert data_stream == [i for i in range(1, 6)]
+
+
+def test_data_stream_pass_generator_as_an_argument_append_full():
+    data_stream = DataStream(range(0, 6), max_length=5)
+    assert data_stream.append(10) is True
 
 
 ###############################
