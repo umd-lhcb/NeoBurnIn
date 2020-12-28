cf9bdaee6c555c7435ac1c96123f24b3b78f5886
Yipeng Sun <syp@umd.edu>
2020-12-28 01:52:26 +0100

    Add curses-menu.

---
+++ nix/curses-menu/default.nix
@@ -0,0 +1,14 @@
+{ stdenv, buildPythonPackage, fetchPypi }:
+
+buildPythonPackage rec {
+  pname = "curses-menu";
+  version = "0.5.0";
+
+  src = fetchPypi {
+    inherit pname version;
+    extension = "zip";
+    sha256 = "29c45e2f16283833e2940fac0fd64e948f2ff603d3e11f510c5bd2b946cd8981";
+  };
+
+  doCheck = false;
+}
